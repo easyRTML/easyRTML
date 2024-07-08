76a85e8ed9c73887dbc2e78ccb74559994e5a848
Aryan Jadhav <easiestrtml@gmail.com>
2024-07-08 12:39:22 +0530

    Create __init__.py

---
+++ easyRTML_PakageContent/easyRTML/__init__.py
@@ -0,0 +1,9 @@
+from .dataaq import DataAQ
+from .process import Processor
+from .plot import Plot
+from .extract import Extractor
+from .xg import XGML
+from .rb import RBML
+from .auth import authenticate
+
+__all__ = ['DataAQ', 'Processor', 'Plot', 'Extractor', 'XGML', 'RBML', 'authenticate']
